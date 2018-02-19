572412e891dfd115657f8d3472cc86652b85b4da
Alejandro <alejandro@MacBook-Pro-de-Alejandro.local>
2018-02-19 11:40:54 +0100

    Login Facebook firebase

--- app/src/main/java/com/example/alejandro/examenpmdm/LoginActivity.java
+++ app/src/main/java/com/example/alejandro/examenpmdm/LoginActivity.java
@@ -18,6 +18,7 @@ import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -57,6 +58,10 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
 
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null){
+                    goMainScreen();
+                }
             }
         };
     }
@@ -67,9 +72,11 @@ public class LoginActivity extends AppCompatActivity {
         firebaseAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
-
+                if(!task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(),"error login", Toast.LENGTH_LONG).show();
+                }
             }
-        })
+        });
     }
 
     private void goMainScreen() {
