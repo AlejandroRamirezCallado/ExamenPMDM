c03237114f262696b3234ee7c3ab4c3798c968fe
Alejandro <alejandro@MacBook-Pro-de-Alejandro.local>
2018-02-19 13:35:01 +0100

    FirebaseAdmin

--- app/src/main/java/com/example/alejandro/examenpmdm/FirebaseAdmin.java
+++ app/src/main/java/com/example/alejandro/examenpmdm/FirebaseAdmin.java
@@ -3,6 +3,41 @@ package com.example.alejandro.examenpmdm;
 /**
  * Created by alejandro on 19/2/18.
  */
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class FirebaseAdmin {
+    private FirebaseAuth mAuth;
+    public FirebaseAdminListener listener;
+
+    public FirebaseAdmin(){
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void setListener(FirebaseAdminListener listener){
+        this.listener=listener;
+    }
+
+    public void loginConEmailYPassword(String email, String pass, Activity activity){
+        mAuth.createUserWithEmailAndPassword(email, pass)
+                .addOnCompleteListener(activity, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d("FirebaseAdmin", "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                        if (task.isSuccessful()) {
+
+                        }else {
+
+                        }
+                    }
+                });
+    }
 }
---
+++ app/src/main/java/com/example/alejandro/examenpmdm/FirebaseAdminListener.java
@@ -0,0 +1,8 @@
+package com.example.alejandro.examenpmdm;
+
+/**
+ * Created by alejandro on 19/2/18.
+ */
+
+public interface FirebaseAdminListener {
+}
